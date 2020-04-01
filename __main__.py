b6815318eed838caa9d238781fecad4ca42785db
ScoopGracie <scoopgracie@gmail.com>
2020-04-01 15:56:02 -0700

    Updates

--- __main__.py
+++ __main__.py
@@ -1,5 +1,23 @@
 import stockquotes
 import json
+simple_graphics = False
+if simple_graphics:
+    _print = print
+    def print(text):
+        text = text\
+            .replace('╭', '┌')\
+            .replace('╮', '┐')\
+            .replace('╰', '└')\
+            .replace('╯', '┘')\
+            .replace('┝', '├')\
+            .replace('━', '─')\
+            .replace('┥', '┤')\
+            .replace('┿', '┼')\
+            .replace('╞', '├')\
+            .replace('═', '─')\
+            .replace('╡', '┤')\
+            .replace('╪', '┼')
+        _print(text)
 
 def pada(string, length):
     string = str(string)
@@ -45,18 +63,43 @@ for stock in portfolio_data:
     length = len(stock[0])
     longest_stock = length if length > longest_stock else longest_stock
 
-header = '│{}│{}│    Last   Total  %Total│'.format(pada('Stock', longest_stock), pad('Qty', longest_qty))
-print('┌{}┐'.format('─' * (len(header)-2)))
-print(header)
-print('├{}┤'.format('─' * (len(header)-2)))
+longest_last = 4
 for stock in portfolio_data:
-    print('{}|{}|{}|{}|{}%'.format(
+    length = len(money(stock[2]))
+    longest_last = length if length > longest_last else longest_last
+
+longest_total = len(money(total)) if len(money(total)) > 5 else 5
+for stock in portfolio_data:
+    length = len(money(stock[2]*stock[1]))
+    longest_total = length if length > longest_total else longest_total
+headings = [ pada('Stock', longest_stock), pad('Qty', longest_qty), pad('Last', longest_last), pad('Total', longest_total), '%Total' ]
+header = '│{}│{}│{}│{}│{}│'.format(*headings)
+print('╭{}┬{}┬{}┬{}┬{}╮'.format( *[ ('─' * len(i)) for i in headings] ))
+print(header)
+print('┝{}┿{}┿{}┿{}┿{}┥'.format( *[ ('━' * len(i)) for i in headings] ))
+for stock in portfolio_data:
+    print('│{}│{}│{}│{}│{}%│'.format(
         pada(stock[0], longest_stock),
         pad(stock[1], longest_qty),
-        pad(money(stock[2]), longest_qty + 1),
-        pad(money(stock[1]*stock[2]), 8),
-        pad(round((stock[1]*stock[2])/total * 100), 7)
+        pad(money(stock[2]), longest_last),
+        pad(money(stock[1]*stock[2]), longest_total),
+        pad(round((stock[1]*stock[2])/total * 100), 5)
     ))
 
-print('Cash                    {}{}%'.format(pad(cash, 8), pad(round((cash/total)*100), 7)))
-print('Total                   {}    100%'.format(pad(money(total), 8)))
+print('├{}┼{}┼{}┼{}┼{}┤'.format( *[ ('─' * len(i)) for i in headings] ))
+print('│{}│{}│{}│{}│{}%│'.format(
+    pada('Cash', longest_stock),
+    ' ' * longest_qty,
+    ' ' * longest_last,
+    pad(money(cash), longest_total),
+    pad(round(cash/total * 100), 5)
+))
+print('╞{}╪{}╪{}╪{}╪{}╡'.format( *[ ('═' * len(i)) for i in headings] ))
+print('│{}│{}│{}│{}│{}%│'.format(
+    pada('Total', longest_stock),
+    ' ' * longest_qty,
+    ' ' * longest_last,
+    pad(money(total), longest_total),
+    pad('100', 5)
+))
+print('╰{}┴{}┴{}┴{}┴{}╯'.format( *[ ('─' * len(i)) for i in headings] ))
